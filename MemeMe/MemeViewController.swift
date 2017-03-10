da7c618926fa85dc983490cdeb82ea508a65a61d
Milad Nozari <mnvoh90@gmail.com>
2017-03-11 00:45:16 +0300

    feat: Added the required code for checking camera access permission.

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MemeViewController.swift
 //  MemeMe
 //
 //  Created by Milad Nozari on 3/10/17.
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MemeViewController: UIViewController {
     
@@ -25,8 +26,16 @@ class MemeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        topText.delegate = self
+        bottomText.delegate = self
+        
+        // disable the camera button if no camera is availble
         if !UIImagePickerController.isSourceTypeAvailable(.camera) {
             cameraButton.isEnabled = false
+        }
+        else {
+            // if camera is available, check to see if we've been granted permission or not.
+            checkForCameraPermission()
         }
     }
     
@@ -47,6 +56,11 @@ class MemeViewController: UIViewController {
     }
     
     @IBAction func loadImageFromCamera(_ sender: UIBarButtonItem) {
+        // load an image from the camera
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func loadImageFromGallery(_ sender: UIBarButtonItem) {
@@ -62,6 +76,22 @@ extension MemeViewController: UITextFieldDelegate {
     
 }
 
+// MARK: - UINavigationControllerDelegate
+extension MemeViewController: UINavigationControllerDelegate {
+    
+    
+    
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension MemeViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+    }
+    
+}
+
 // MARK: - Public functions
 extension MemeViewController {
     
@@ -73,7 +103,55 @@ extension MemeViewController {
 // MARK: - Private functions
 extension MemeViewController {
 
+    fileprivate func checkForCameraPermission() {
+        // get the current authorization status of camera access
+        let permissionStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        
+        switch permissionStatus {
+        case .authorized:
+            // Already authorized. Nothing to do.
+            break
+        case .denied:
+            // The user has previously opted to deny access, 
+            // so we'll ask her to open settings and grant it.
+            requestCameraAccess(alreadyMade: true)
+        case .notDetermined:
+            // The status is not determined, i.e. it has neither been
+            // granted nor denied, which means it's the first time this
+            // process is occuring. So ask for permission
+            requestCameraAccess(alreadyMade: false)
+        default:
+            break
+        }
+    }
     
+    fileprivate func requestCameraAccess(alreadyMade: Bool) {
+        if alreadyMade {
+            let title = "Camera Permission"
+            let message = alreadyMade
+                ? "You have denied access to your camera. Please open your device's settings to grant the permission."
+                : "We need to access your camera, in case you want to take a picture and make a meme from it."
+            let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let actionOpenSettings = UIAlertAction(title: "Open Settings", style: .default, handler: openSettings)
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alertController.addAction(actionCancel)
+            alertController.addAction(actionOpenSettings)
+            present(alertController, animated: true, completion: nil)
+        }
+        else {
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: nil)
+        }
+    }
+    
+    fileprivate func openSettings(_ action: UIAlertAction) {
+        guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+            return
+        }
+        // Open the url of the device's settings.
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: nil)
+        }
+    }
     
 }
 
