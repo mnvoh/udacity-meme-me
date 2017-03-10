760c38b1517788055d2d86e27582c96a9b5e8074
Milad Nozari <mnvoh90@gmail.com>
2017-03-10 23:59:48 +0300

    feat: Added some method stubs and added IBOutlets and IBActions.

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class MemeViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Overrides
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // TODO: Add keyboard observer
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // TODO: Remove keyboard observer
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func share(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func loadImageFromCamera(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func loadImageFromGallery(_ sender: UIBarButtonItem) {
+    }
+}
+
+// MARK: - Public functions
+extension MemeViewController {
+    
+    
+    
+}
 
 
+// MARK: - Private functions
+extension MemeViewController {
+
+    
+    
 }
 
