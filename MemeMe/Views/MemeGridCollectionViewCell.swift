2e1d3fb53d891bdc6fec7cc5024ef4ee7df5d5f5
Milad Nozari <mnvoh90@gmail.com>
2017-03-12 20:26:03 +0300

    feat: Created a UICollectionViewCell for the grid view cells

---
+++ MemeMe/Views/MemeGridCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeGridCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Milad Nozari on 3/12/17.
+//  Copyright © 2017 Nozary. All rights reserved.
+//
+
+import UIKit
+
+class MemeGridCollectionViewCell: UICollectionViewCell {
+    
+    var memeImage: UIImageView!
+    var topText: UILabel!
+    var bottomText: UILabel!
+    
+}
